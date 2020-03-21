c740b27d54f479e230cf247360c05a7072955041
lsc <stark_lu@foxmail.com>
2020-03-21 11:11:17 +0800

    fix making dataset (no scale problem)

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #python trajectory_extraction.py
 python trajectory_segmentation_and_features.py
 python MF_RP_mat.py
-python PEDCC.py
-python dataset.py
-python Dual_SAE.py
+#python PEDCC.py
+#python dataset.py
+#python Dual_SAE.py
--- test/visualize_data.py
+++ test/visualize_data.py
@@ -1,9 +1,26 @@
+import os
+
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.model_selection import train_test_split
 
-features_segments = np.load('./geolife/features_segments.npy')[:]
-features_segments_labels = np.load('./geolife/features_segments_labels.npy')[:]
+from params import N_CLASS
+from utils import visualizeData
 
-fs = features_segments[:,:,0].flatten()
-plt.hist(fs, bins='auto')
-plt.show()
+trjs_segs_features = np.load('../data/geolife_features/trjs_segs_features.npy')
+trjs_segs_features = np.reshape(trjs_segs_features, (trjs_segs_features.shape[0],-1))
+labels = np.load('../data/geolife_features/trjs_segs_features_labels.npy')
+x_features_series_train, x_features_series_test, \
+y_train, y_test \
+    = train_test_split(
+    trjs_segs_features,
+    labels,
+    test_size=0.20, random_state=7, shuffle=True
+)
+np.save('../data/geolife_features/x_features_series_train.npy', x_features_series_train)
+np.save('../data/geolife_features/x_features_series_test.npy', x_features_series_test)
+np.save('../data/geolife_features/y_train.npy', y_train)
+np.save('../data/geolife_features/y_test.npy', y_test)
+
+
+visualizeData(x_features_series_test, y_test, N_CLASS,  'raw_data.png')
